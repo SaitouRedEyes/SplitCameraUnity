e74ed2cabc04bed9745e59ea7325b0cced69609c
SaitouRedEyes <zeca.saitou@gmail.com>
2017-05-25 14:03:32 -0300

    Example of a Split Camera in Unity
    A example of a split camera behaviour in a 2 player local game in Unity.

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour 
+{
+    [SerializeField]
+    private Camera cameraP1, cameraP2;
+    [SerializeField]
+    private GameObject centerLine;
+
+    private float limitBetweenPlayers;
+    
+	// Use this for initialization
+	void Start () 
+    {
+        limitBetweenPlayers = 6.7f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float distance = Vector3.Distance(cameraP1.transform.position, cameraP2.transform.position);
+        this.transform.position = 0.5f * (cameraP1.transform.position + cameraP2.transform.position);
+
+        if(distance < limitBetweenPlayers)
+        {
+            this.GetComponent<Camera>().enabled = true;
+            centerLine.gameObject.SetActive(false);
+        }
+        else
+        {
+            this.GetComponent<Camera>().enabled = false;
+            centerLine.gameObject.SetActive(true);
+        }
+	}
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour 
+{
+    protected float Fps;
+
+    [SerializeField]
+    protected float Speed;
+
+    [SerializeField]
+    protected string MoveX, MoveY, BtShoot;
+    
+	// Use this for initialization
+	void Start () 
+    {
+   
+	}
+
+    protected void Move()
+    {
+        Fps = Time.deltaTime * 60;
+
+        this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * Fps * this.Speed, Input.GetAxisRaw("Vertical") * Fps * this.Speed);
+    }
+
+    /*protected void Shoot()
+    {
+        if(Input.GetAxisRaw)
+        {
+
+        }
+    }*/
+
+	// Update is called once per frame
+	void Update () 
+    {
+        Move();
+	}
+}
